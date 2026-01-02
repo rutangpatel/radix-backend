1bd7fbfb09c8271b2f1923868bcb48f5554cf053
Rutang Patel <rutangpatel0204@gmail.com>
2026-01-02 12:16:12 +0530

    Deleted unwanted Index in MongoDB

--- app/payment.py
+++ app/payment.py
@@ -10,8 +10,8 @@ router = APIRouter()
 
 radix = get_database()
 transactions = radix["transactions"]
-index1 = IndexModel([("id",ASCENDING)], unique = True)
-index2 = IndexModel([("mob_no",ASCENDING)], unique = True)
+index1 = IndexModel([("id")])
+index2 = IndexModel([("transaction_id", ASCENDING)], unique = True)
 category_index = transactions.create_indexes([index1, index2])
 
 
